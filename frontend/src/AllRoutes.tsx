4a70bbdef680b68516e782753a779f4ab53fd1fb
Hasim Shaikh <hasims2001@gmail.com>
2023-08-31 20:09:34 +0530

    frontend added

---
+++ frontend/src/AllRoutes.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Category } from './pages/Category'
+
+export const AllRoutes = () => {
+  return (
+    <Routes>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/category" element={<Category />}></Route>
+</Routes>
+  )
+}
