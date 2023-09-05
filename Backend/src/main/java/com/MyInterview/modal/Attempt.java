8e25b6a769dbbeb2b6a68cfbb9f8e924cbbaaf92
Amit0841 <aroy16063@gmail.com>
2023-09-05 12:42:07 +0530

    Java backend

--- Backend/src/main/java/com/MyInterview/modal/Attempt.java
+++ Backend/src/main/java/com/MyInterview/modal/Attempt.java
@@ -1,7 +1,5 @@
 package com.MyInterview.modal;
 
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
